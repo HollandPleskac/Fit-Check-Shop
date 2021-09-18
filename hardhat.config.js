c36cea2e0946ec8cd06541ae5f4ddb65d81e48da
Holland Pleskac <hollandpleskac@gmail.com>
2021-09-18 13:20:28 -0700

    credentials

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
+const { task } = require("hardhat/config");
+
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config({ path: '.env.local' })
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,6 +14,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+task("credentials", "Prints the private key and infura endpoint", async (taskargs, hre) => {
+  console.log('infura endpoint', process.env.INFURA_ENDPOINT)
+  console.log('private key', process.env.META_MASK_PRIVATE_KEY)
+})
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -18,4 +27,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  // networks: {
+  //   hardhat: {
+  //     chainId: 1337 // make sure everything works
+  //   },
+  //   rinkeby: {
+  //     url: process.env.INFURA_ENDPOINT,
+  //     accounts: [`0x${process.env.META_MASK_PRIVATE_KEY}`]
+  //   }
+  // }
 };
